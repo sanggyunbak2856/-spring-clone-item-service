10c802fca9c43ba35f1ecb377c02e4c082352006
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-09-24 13:46:02 +0900

    feat: 상품목록페이지를 동적페이지로 변경 BasicItemController에서 받은 상품목록으로 동적으로 렌더링하도록 변경 타임리프 사용함

--- src/main/java/hello/itemservice/domain/item/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/domain/item/web/basic/BasicItemController.java
@@ -22,7 +22,7 @@ public class BasicItemController {
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
-        return "basic/item";
+        return "basic/items";
     }
 
     /**
